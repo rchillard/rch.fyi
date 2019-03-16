92ef38327dbf09a21ea74cdbd9fb6966222c57ef
RCH <rchillard@gmail.com>
2019-03-16 09:32:54 -0500

    Update hours of the day for dawn and dusk in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 var bg = document.body;
 var hour = new Date().getHours();
 
-if (hour >= 5 && hour <= 7) {
+if (hour >= 5 && hour <= 8) {
   bg.classList.add("dawn");
-} else if (hour > 7 && hour < 18) {
+} else if (hour > 8 && hour < 18) {
   bg.classList.add("day");
-} else if (hour >= 18 && hour <= 20) {
+} else if (hour >= 18 && hour <= 21) {
   bg.classList.add("dusk");
 } else {
   bg.classList.add("night");
