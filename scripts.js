a768acb782a2d355f6d1dfc3e041f8a307890f12
Ryan <rchillard@gmail.com>
2019-03-16 09:13:26 -0500

    Add sky.html and 1st attempt at programmatic starfield

--- scripts.js
+++ scripts.js
@@ -10,3 +10,31 @@ if (hour >= 5 && hour <= 7) {
 } else {
   bg.classList.add('night')
 }
+
+// check if night and draw stars
+if (bg.className == 'night') {
+  console.log('Night falls on sky.html')
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+  
+  function drawStar(x, y, size, canvas){
+    canvas.strokeStyle = "whitesmoke";
+    canvas.strokeRect(x,y,size,size);
+  }
+  
+  function redraw() {
+    var canvas = document.getElementById("stars");
+    var context = canvas.getContext("2d");
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+  
+    // Draw stars
+    for(i = 0; i < 100; i++) {
+      drawStar(getRndInteger(0, canvas.width), getRndInteger(0, canvas.height), 1, context)
+    }
+  }
+  
+  window.addEventListener("resize", redraw);
+  redraw();
+}
