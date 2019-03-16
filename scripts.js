fe8db1d1327c6eb58324f1667659ffe95b9d23c3
RCH <rchillard@gmail.com>
2019-03-16 13:33:28 -0500

    Add footer, ground, and rising and setting suns as new canvas

--- scripts.js
+++ scripts.js
@@ -11,7 +11,10 @@ if (hour >= 5 && hour <= 8) {
   bg.classList.add("night");
 }
 
-// check if night and draw stars
+// TESTING
+// bg.classList.add("dusk");
+
+// Check if night and draw stars
 if (bg.className == "night") {
   console.log("Night falls on sky.html");
   function getRndInteger(min, max) {
@@ -42,4 +45,77 @@ if (bg.className == "night") {
 
   window.addEventListener("resize", redraw);
   redraw();
+}
+
+// This needs to be re-factored for simplicity
+// Check if dawn and draw rising sun
+if (bg.className == "dawn") {
+  function convertRemToPixels(rem) {
+    return (
+      rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
+    );
+  }
+
+  function draw() {
+    // Half circle
+    var canvas = document.getElementById("sunrise");
+    if (canvas.getContext) {
+      var context = canvas.getContext("2d");
+      var x = convertRemToPixels(2);
+      var y = convertRemToPixels(2);
+      console.log(y);
+      var radius = convertRemToPixels(2);
+      var startAngle = 0;
+      var endAngle = Math.PI;
+      var antiClockwise = true;
+      context.beginPath();
+      context.arc(x, y, radius, startAngle, endAngle, antiClockwise);
+      context.closePath();
+
+      var gradient = context.createLinearGradient(16, 32, 16, 0);
+      gradient.addColorStop(0, "#ff512f");
+      gradient.addColorStop(1, "#f09819");
+
+      context.fillStyle = gradient;
+      context.fill();
+    }
+  }
+
+  draw();
+}
+
+// Check if dusk and draw setting sun
+if (bg.className == "dusk") {
+  function convertRemToPixels(rem) {
+    return (
+      rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
+    );
+  }
+
+  function draw() {
+    // Half circle
+    var canvas = document.getElementById("sunset");
+    if (canvas.getContext) {
+      var context = canvas.getContext("2d");
+      var x = convertRemToPixels(2);
+      var y = convertRemToPixels(2);
+      console.log(y);
+      var radius = convertRemToPixels(2);
+      var startAngle = 0;
+      var endAngle = Math.PI;
+      var antiClockwise = true;
+      context.beginPath();
+      context.arc(x, y, radius, startAngle, endAngle, antiClockwise);
+      context.closePath();
+
+      var gradient = context.createLinearGradient(16, 32, 16, 0);
+      gradient.addColorStop(0, "#f05053");
+      gradient.addColorStop(1, "#fc500c");
+
+      context.fillStyle = gradient;
+      context.fill();
+    }
+  }
+
+  draw();
 }
